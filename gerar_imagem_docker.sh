74c17d3803d239f171cb2ac2420773cd2b62e369
davicarrano <davicarrano@yahoo.com.br>
2024-03-12 10:18:10 -0300

    corrigi o script de geração da imagem docker

--- gerar_imagem_docker.sh
+++ gerar_imagem_docker.sh
@@ -1,22 +1,7 @@
 IMAGEM=registry.ufsj.edu.br:5999/frontend/smartglpi-web
 VERSAO=v1.0.0
 
-if [[ "$1" = "homolog" ]] 
-then
-    npm run build --configuration homolog  
-    docker build -t $IMAGEM-homolog:$VERSAO .
-    docker push $IMAGEM-homolog:$VERSAO
-
-elif [[ "$1" = "prod" ]]
-then
-    npm run build 
-    docker build -t $IMAGEM:$VERSAO .
-    docker push $IMAGEM:$VERSAO
-else
-    echo "É preciso informar o ambiente para geração do container: homolog ou prod"
-fi
-
-
-
+docker build -t $IMAGEM:$VERSAO .
+docker push $IMAGEM:$VERSAO
 
 
